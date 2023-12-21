80e786e40f5f685158250a56a3214228641ab919
Andrii Sorochak <a.sorochak@mmachine.net>
2023-12-21 09:03:30 +0200

    test deploy.

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.85.0"
+        source = "hashicorp/azurerm"
+        version = "=3.85.0"
     }
   }
 
   backend "azurerm" {
-    resource_group_name     = "tfstorage"
-    storage_account_name    = "tfstoraccountnerd"
-    container_name          = "statefile"
-    key                     = "terraform.state"
+    resource_group_name = "tfstorage"
+    storage_account_name = "tfstoraccountnerd"
+    container_name = "statefile"
+    key = "terraform.state"
   }
 }
 
@@ -19,30 +19,27 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name      = "my_resource_group"
-  location  = "Poland Central"
+  location = "Poland Central"
+  name     = "my_resoource_group"
 }
 
-
 resource "azurerm_service_plan" "sp" {
-  name                  = "my-service-plan"
+  name                  = "my_service_plan"
   resource_group_name   = azurerm_resource_group.rg.name
-  location              =  azurerm_resource_group.rg.location
+  location              = azurerm_resource_group.rg.location
   os_type               = "Linux"
   sku_name              = "F1"
 }
 
-resource "azurerm_linux_web_app" "example" {
- name                   = "nerdysoft-terraform-linux-app"
- resource_group_name    = azurerm_resource_group.rg.name
- location               = azurerm_service_plan.sp.location
- service_plan_id        = azurerm_service_plan.sp.id
- 
- site_config {
+resource "azurerm_linux_web_app" "wa" {
+  name                  = "nerdysoft-terraform-linux-app"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_service_plan.sp.location
+  service_plan_id       = azurerm_service_plan.sp.id
+  site_config {
     application_stack {
-        dotnet_version = "7.0"
+      dotnet_version = "7.0"
     }
- 
-	always_on  = false
- }
+    always_on = false
+  }
 }
